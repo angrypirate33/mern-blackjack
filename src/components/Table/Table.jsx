02e0d697cde180edbdbf0e383cbd0471ef69f0b7
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-18 17:05:37 -0400

    Creates framework for Table component, housing all other related components.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,8 +1,21 @@
 
+import BankrollInfo from '../BankrollInfo/BankrollInfo'
+import DealerCards from '../DealerCards/DealerCards'
+import PlayerActions from '../PlayerActions/PlayerActions'
+import PlayerCards from '../PlayerCards/PlayerCards'
+import ScoreDisplay from '../ScoreDisplay/ScoreDisplay'
+import WagerInfo from '../WagerInfo/WagerInfo'
 import '../../components/Table/Table.css'
 
 export default function Table() {
     return (
-        <h2>Table</h2>
+        <div className='Table'>
+            <ScoreDisplay />
+            <DealerCards />
+            <WagerInfo />
+            <ScoreDisplay />
+            <PlayerCards />
+            <PlayerActions />
+        </div>
     )
 }
