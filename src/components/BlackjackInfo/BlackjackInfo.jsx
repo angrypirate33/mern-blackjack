f8cdf525a20cd25dbe79b629544670af2bc9cca1
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-17 22:40:32 -0400

    Adds house rules and a button to make the BlackjackInfo component collapsable.

---
+++ src/components/BlackjackInfo/BlackjackInfo.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import '../../components/BlackjackInfo/BlackjackInfo.css'
+
+export default function BlackjackInfo() {
+    const [rulesVisible, setRulesVisible] = useState(true)
+
+    const toggleRulesVisibility = () => {
+        setRulesVisible(!rulesVisible)
+    }
+
+    return (
+        <div className='BlackjackInfo'>
+            <h1 id='pageTitle'>Blackjack - 21</h1>
+            {rulesVisible && (
+                <div id='hosueRules'>
+                    <h3 id='rulesTitle'>House Rules:</h3>
+                            <ul id='rulesDetail'>
+                                <li>Min bet - $10 Max Bet - $1,000.</li>
+                                <li>Ten deck shoe.</li>
+                                <li>Dealer hits on 16 and stays on 17+.</li>
+                                <li>Blackjack pays 3:2.</li>
+                                <li>Number cards - face value, face cards - 10, Aces - 11</li>
+                                <li>Splitting hands is not allowed at this time.</li>
+                                <li>Insurance is not available.</li>
+                            </ul>
+                        </div>
+                    )}
+                    <button 
+                        className='waves-effect waves-light btn green darken-4'
+                        onClick={toggleRulesVisibility}>
+                            {rulesVisible ? 'Hide Rules' : 'Show Rules'}
+                    </button>
+                </div>
+    )
+}
--- src/pages/BlackjackPage/BlackjackPage.jsx
+++ src/pages/BlackjackPage/BlackjackPage.jsx
@@ -1,10 +1,10 @@
-
+import BlackjackInfo from '../../components/BlackjackInfo/BlackjackInfo'
 import '../../pages/BlackjackPage/BlackjackPage.css'
 
 export default function BlackjackPage() {
     return (
         <div className='BlackjackPage'>
-            <h2>BlackjackPage</h2>
+            <BlackjackInfo />
         </div>
     )
 }
