33066fa3544b6bfbe5ea5df36546a29ce6259b90
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-07-19 11:53:31 -0400

    Adds basic hit and stand buttons.

--- src/components/PlayerActions/PlayerActions.jsx
+++ src/components/PlayerActions/PlayerActions.jsx
@@ -4,7 +4,18 @@ import '../../components/PlayerActions/PlayerActions.css'
 export default function PlayerActions() {
     return (
         <div className='PlayerActions'>
-            <h4>PlayerActions</h4>
+            <button
+                className='waves-effect waves-light btn-large red darken-4'
+                id='hit-button'
+            >
+                Hit
+            </button>
+            <button
+                className='waves-effect waves-light btn-large red darken-4'
+                id='stand-button'
+            >
+                Stand
+            </button>
         </div>
     )
 }
