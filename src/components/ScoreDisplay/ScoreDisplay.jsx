2eba83e417d366e68c2d928ba4af7c4f9bf33f85
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-08-14 10:36:55 -0400

    Fixes issue with empty elements shifting.  CSS layout complete for large screens.

--- src/components/ScoreDisplay/ScoreDisplay.jsx
+++ src/components/ScoreDisplay/ScoreDisplay.jsx
@@ -4,8 +4,8 @@ import './ScoreDisplay.css'
 export default function ScoreDisplay({ title, score, className }) {
     return (
         <div className={`ScoreDisplay ${className}`}>
-            <h4>{title}</h4>
-            <h4>{score?.total}</h4>
+            <h4 id='title'>{title}</h4>
+            <h4 id='score'>{score?.total}</h4>
         </div>
 
     )
