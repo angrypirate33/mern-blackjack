609cf388308d0969993112a4a33ef2c82a33fd42
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-08-21 12:41:35 -0400

    Refactors nav for large screens in preperation of adding sidenav for small screens.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,42 +1,53 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import * as userService from '../../utilities/users-service'
 import '../../components/Nav/Nav.css'
 
 export default function Nav({ user, setUser }) {
+
+    const [sidenavInstance, setSidenavInstance] = useState(null)
+
     function handleLogOut() {
         userService.logOut()
         setUser(null)
     }
 
+    function closeSidenav() {
+        if (sidenavInstance) {
+            sidenavInstance.close()
+        }
+    }
+
+    useEffect(() => {
+        let elems = document.querySelectorAll('.sidenav')
+        let instances = window.M.Sidenav.init(elems, {})
+        setSidenavInstance(instances[0])
+    }, [])
+
     return (
-        <nav className='green darken-4'>
-            <div className='nav-wrapper'>
-                <span id='welcome-message'>Welcome, {user.name}</span>&nbsp;&nbsp;
-                |&nbsp;&nbsp;&nbsp;
-                <Link
-                    to='/'
-                >
-                    Play Blackjack
-                </Link>
-                &nbsp;&nbsp;&nbsp;
-                {!user.isGuest && (
-                    <>
-                        |&nbsp;&nbsp;&nbsp;
-                        <Link
-                            to='/history'
-                        >
-                            View Hand History
-                        </Link>
-                    </>
-                )}
-                <Link 
-                    className='right' 
-                    id='logout-btn'
-                    to='' onClick={handleLogOut}
-                >
-                    <span id='logout'>Log Out</span>
-                </Link>
-            </div>
-        </nav>
+        <div className='Nav'>
+            <nav className='green darken-4'>
+                <div className='nav-wrapper'>
+                    <a href='#' data-target='sidenav-links' className='sidenav-trigger left'><i className='material-icons'>menu</i></a>
+                    <span id='welcome-message'>Welcome, {user.name}</span>&nbsp;&nbsp;&nbsp;
+                    <ul className='hide-on-med-and-down nav-links'>
+                        <li><Link to='/' > Play Blackjack </Link></li>
+                        <li>
+                            {!user.isGuest && (
+                                <Link to='/history' > View Hand History </Link>
+                            )}
+                        </li>
+                    </ul>
+                    <Link
+                        className='hide-on-med-and-down'
+                        id='logout-btn'
+                        to='' onClick={handleLogOut}
+                    >
+                        <span id='logout'>Log Out</span>
+                    </Link>
+                </div>
+            </nav>
+        </div>
+
     )
 }
